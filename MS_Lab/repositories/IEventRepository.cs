efed280027ac40c9a1b3719e98c4595ca96513ab
Zimin Egor <ziminson@yandex.ru>
2026-02-12 20:33:51 +0300

    feat: Описаны интерфейсы репозиториев

---
+++ MS_Lab/repositories/IEventRepository.cs
@@ -0,0 +1,16 @@
+﻿using MS_Lab.entities;
+
+namespace MS_Lab.repositories
+{
+
+    public interface IEventRepository
+    {
+        Task<IEnumerable<Event>> GetAllAsync();
+        Task<Event?> GetByIdAsync(int id);
+        Task<Event> CreateAsync(Event eventEntity);
+        Task<Event> UpdateAsync(Event eventEntity);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsByIdAsync(int id);
+    }
+
+}
---
+++ MS_Lab/repositories/ITicketRepository.cs
@@ -0,0 +1,16 @@
+﻿using MS_Lab.entities;
+
+namespace MS_Lab.repositories
+{
+
+    public interface ITicketRepository
+    {
+        Task<IEnumerable<Ticket>> GetAllAsync();
+        Task<Ticket?> GetByIdAsync(int id);
+        Task<Ticket> CreateAsync(Ticket ticket);
+        Task<Ticket> UpdateAsync(Ticket ticket);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsByIdAsync(int id);
+        Task<int> GetSoldCountByEventIdAsync(int eventId);
+    }
+}
